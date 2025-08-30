928237142eb7cd4cde2ab82d2e4ecdb19e55d676
Prasadk08 <prasadkshirsagar1199@gmail.com>
2025-08-30 10:26:23 +0530

    reloading backend

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const secret =
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "https://workout-frontend-z6e3.vercel.app/",
+    origin: "https://workout-frontend-z6e3.vercel.app",
     credentials: true,
   })
 );
