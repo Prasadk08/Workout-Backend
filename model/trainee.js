c3295121f616afbe08779c662e53ec2010424200
Prasadk08 <prasadkshirsagar1199@gmail.com>
2025-09-04 22:33:26 +0530

    ai integration

--- model/trainee.js
+++ model/trainee.js
@@ -1,25 +1,30 @@
-import mongoose from'mongoose';
+import mongoose from 'mongoose';
 
 const TraineeSchema = new mongoose.Schema({
     name: { type: String },
-    address:{type:String},
+    address: { type: String },
     phone: { type: Number },
-    gymDetails:{
+
+    gender: { type: String, enum: ["Male", "Female", "Other"], default: "Other" },
+    age: { type: Number },
+    personalInfo: { type: String },
+
+    gymDetails: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Owner',
     },
-    myPlan:{
+    myPlan: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'GymPlan',
     },
-    startDate:{ type: Date },
-    endDate:{ type: Date },
+    startDate: { type: Date },
+    endDate: { type: Date },
     isActive: { type: Boolean, default: false },
-    pastjoinings:{
+    pastjoinings: {
         type: [mongoose.Schema.Types.ObjectId],
         ref: 'PastJoining',
     }
-})
+});
 
 const Trainee = mongoose.model('Trainee', TraineeSchema);
 export default Trainee;
